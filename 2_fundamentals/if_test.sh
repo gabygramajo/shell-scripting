bed7adbe311d2a3a82c7e9a4477179122d085e7c
gabriel <ggabrieldeveloper@gmail.com>
2025-03-15 22:53:05 -0300

    add: 2_fundamentals

---
+++ 2_fundamentals/if_test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+var1="cadenatexto"
+
+#Estructura if
+# [] hace referencia a un programa en /usr/bin/test que sirve para comparar tipos de datos.
+if [ $var1 = "cadenatexto" ]; then
+        echo "Cadenas iguales"
+else
+        echo "Cadena diferenes"
+fi
+
+#Ejemplo 2
+if [ $var1 = "cadenatexto" ]; then
+        echo "Cadenas iguales"
+else
+        echo "Cadena diferenes"
+fi
+
+#Ejemplo 2
+if [ $var1 = "cadenatexto" ]; then
+        echo "Cadenas iguales"
+else
+        echo "Cadena diferenes"
+fi
